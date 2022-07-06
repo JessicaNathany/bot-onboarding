4e91ae1a0ca5b596481030b8873bb750fd27eb93
Jéssica Nathany <jessicanathany.web@gmail.com>
2022-07-06 12:26:21 -0300

    primeiro commit no bot

--- bot-overboarding/bot-onboarding/Bots/EchoBot.cs
+++ bot-overboarding/bot-onboarding/Bots/EchoBot.cs
@@ -10,13 +10,65 @@ namespace bot_onboarding.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var replyText = $"Bot Onboarding: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            var messageRecebida = $"Bot Onboarding: {turnContext.Activity.Text}";
+            
+            if (InformacaoUsuario.nome == null)
+            {
+                InformacaoUsuario.nome = turnContext.Activity.Text;
+                InformacaoUsuario.mensagem = $"Olá, {InformacaoUsuario.nome}! Qual seu time?";
+                
+                await turnContext.SendActivityAsync(MessageFactory.Text(InformacaoUsuario.mensagem, InformacaoUsuario.mensagem), cancellationToken);
+                return;
+            }
+
+            if (InformacaoUsuario.time == null && InformacaoUsuario.nome != null)
+            {
+                InformacaoUsuario.time = turnContext.Activity.Text;
+                InformacaoUsuario.mensagem = $"Legal, você vai amar o time {InformacaoUsuario.time}!";
+
+                await turnContext.SendActivityAsync(MessageFactory.Text(InformacaoUsuario.mensagem, InformacaoUsuario.mensagem), cancellationToken);
+
+                var selecioneOpcoesTime = "Digite o número da sua função no time: \r\n  1- Desenvolvedor \r\n 2- UX \r\n 3- QA";
+                await turnContext.SendActivityAsync(MessageFactory.Text(selecioneOpcoesTime, selecioneOpcoesTime), cancellationToken);
+                return;
+
+                if()
+
+                //for (int i = 0; i < 3; i++)
+                //{
+                //    var papel = string.Empty;
+
+                //    switch(i)
+                //    {
+                //        case 0:
+                //            papel = " 1- Desenvolvedor";
+                //            break;
+
+                //        case 1:
+                //            papel = "2- UX";
+                //            break;
+
+                //        case 2:
+                //            papel = "3- QA";
+                //            break;
+                //    }
+
+                //    await turnContext.SendActivityAsync(MessageFactory.Text(papel, InformacaoUsuario.mensagem), cancellationToken);
+                //}
+
+                
+                
+                return;
+            }
+
+
+           
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Olá seja bem-vindo! Qual seu login?";
+            var welcomeText = "Olá seja bem-vindo! Qual seu nome?";
+
 
             foreach (var member in membersAdded)
             {
---
+++ bot-overboarding/bot-onboarding/Bots/InformacaoUsuario.cs
@@ -0,0 +1,19 @@
+﻿namespace bot_onboarding.Bots
+{
+    public static class InformacaoUsuario
+    {
+        public static string nome;
+        public static string time;
+        public static string funcao;
+        public static string mensagem;
+        public static TipoPapel tipoPapel;
+    }
+
+    public enum TipoPapel
+    {
+        Dev = 1,
+        UX = 2,
+        QA = 3
+    }
+}
+
